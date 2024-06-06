f32c4071bbbd5c8146583b11ae66f9af8dd3fd0d
Katherine Kelly <ktkelly11@gmail.com>
2024-06-05 21:33:36 -0400

    Adopter model added

---
+++ models/Adopter.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("../config/db-connection");
+
+const adopterSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  phoneNum: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 9,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 21,
+    max: 100,
+  },
+  otherPets: [
+    {
+      type: Boolean,
+      required: true,
+      pets: {
+        petName: String,
+        petType: String,
+        color: String,
+        age: Number,
+      },
+    },
+  ],
+});
+
+module.exports = model("Adopter", adopterSchema);
