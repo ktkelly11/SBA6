864a63bd20126e4d78df4135e1db8f0978e0b6ca
Katherine Kelly <ktkelly11@gmail.com>
2024-06-05 21:42:28 -0400

    Cat model added

---
+++ models/Cat.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("../config/db-connection");
+
+const catSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  catType: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  vacinated: {
+    type: Boolean,
+    required: true,
+  },
+  referenceNum: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = model("Cat", catSchema);
